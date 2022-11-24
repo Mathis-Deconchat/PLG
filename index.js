6517372111052463051483ec6b2385781c62515a
mathis.deconchat@gmail.com <mathis.deconchat@webinage.fr>
2022-11-24 23:18:52 +0100

    PLG STACK

---
+++ index.js
@@ -0,0 +1,38 @@
+'use strict'
+const winston = require('winston');
+const fs = require('fs');
+const path = require('path');
+
+const express = require('express')
+const app = express()
+const port = 6969
+
+// 🔔 Les deux fichiers doivent être dans le dossier log du projet
+const errorFile = 'log/ynov-error.log';
+const infoFile = 'log/ynov-info.log'
+
+
+// Créer un trasnports winston qui écrit dans le fichier définit dans la var infofile que c'est un log d'info
+// Dans le même logger créer un trasnports qui écrit dans le fichier définit dans errorFile si c'est en erreur
+// C'est dans le doc ;) https://github.com/winstonjs/winston
+const logInfo = winston.createLogger({
+
+})
+
+
+// Exemple d'écriture de log avec un niveau info
+logInfo.info({
+    message: 'message',
+    label: 'label'
+
+})
+
+
+
+// Créer 2 endpoints express, un '/info' et un '/error' qui loggerons chacun dans le bon fichier
+
+
+app.listen(port, () => {
+    console.log(`🚀🚀 ${port}`)
+})
+
